96eca0b055521701c859f92acda1ddc4248e5849
Bi Wu <alerk_star@outlook.com>
2022-03-21 00:00:43 +0800

    init;

---
+++ README.md
@@ -0,0 +1,7 @@
+
+## Source Code
+
+You can get started by baking [this repo](https://github.com/PatrickAlphaC/nft-mix).
+```bash
+brownie bake nfx-mix
+```
---
+++ contracts/SimpleCollectible.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.6.6;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract SimpleCollectible is ERC721 {
+    constructor() ERC721("MyNFT", "MNFT") public {
+    }
+}
+
+
