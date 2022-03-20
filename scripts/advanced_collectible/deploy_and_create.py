5fa9b1295d9dfb031d20f71654eba521050bf746
Bi Wu <alerk_star@outlook.com>
2022-03-21 02:38:06 +0800

    deploying Advancedcollectible;

--- scripts/advanced_collectible/deploy_and_create.py
+++ scripts/advanced_collectible/deploy_and_create.py
@@ -1,18 +1,24 @@
-from scripts.helpful_scripts import get_account
-from brownie import SimpleCollectible
+from scripts.helpful_scripts import get_account, OPENSEA_URL, get_contract
+from brownie import AdvancedCollectible, config, network
 
-sample_token_uri = "ipfs://Qmd9MCGtdVz2miNumBHDbvj8bigSgTwnr4SbyH6DNnpWdt?filename=0-PUG.json"
-OPENSEA_URL = "https://testnets.opensea.io/assets/{}/{}"
+sample_token_uri = (
+    "ipfs://Qmd9MCGtdVz2miNumBHDbvj8bigSgTwnr4SbyH6DNnpWdt?filename=0-PUG.json"
+)
 
 
 def main():
-  deploy_and_create()
+    deploy_and_create()
 
 
 def deploy_and_create():
-  account = get_account()
-  simple_collectible = SimpleCollectible.deploy({"from": account})
-  tx = simple_collectible.createCollectible(sample_token_uri, {"from": account})
-  tx.wait(1)
-  print(f"Awesome, you can view your NFT at {OPENSEA_URL.format(simple_collectible.address, simple_collectible.tokenCounter() - 1)}")
-  return simple_collectible
+    account = get_account()
+    advanced_collectible = AdvancedCollectible.deploy(
+        get_contract("vrf_coordinator").address,
+        get_contract("link_token").address,
+        config["networks"][network.show_active()]["keyhash"],
+        config["networks"][network.show_active()]["fee"],
+        {"from": account},
+    )
+
+    print(f"AdvancedCollectible has been deployed at {advanced_collectible.address}")
+    return advanced_collectible
--- scripts/helpful_scripts.py
+++ scripts/helpful_scripts.py
@@ -1,4 +1,12 @@
-from brownie import accounts, network, config
+from brownie import (
+    accounts,
+    network,
+    config,
+    Contract,
+    MockV3Aggregator,
+    VRFCoordinatorMock,
+    LinkToken,
+)
 
 LOCAL_BLOCKCHAIN_ENVIRONMENTS = [
     "hardhat",
@@ -8,7 +16,17 @@ LOCAL_BLOCKCHAIN_ENVIRONMENTS = [
     "mainnet-fork",
 ]
 FORKED_LOCAL_ENVIRONMENTS = ["mainnet-fork", "mainnet-fork-dev"]
-ETH_AMOUNT_DEPOSIT = 0.05;
+OPENSEA_URL = "https://testnets.opensea.io/assets/{}/{}"
+
+contract_to_mock = {
+    "eth_usd_price_feed": MockV3Aggregator,
+    "vrf_coordinator": VRFCoordinatorMock,
+    "link_token": LinkToken,
+}
+
+
+DECIMALS = 8
+INITIAL_VALUE = 200000000000
 
 
 def get_account(index=None, id=None):
@@ -26,3 +44,34 @@ def get_account(index=None, id=None):
         return accounts[0]
 
     return accounts.add(config["wallets"]["from_key"])
+
+
+def get_contract(contract_name):
+    """This functin will grab the contract addresses from the brownie config
+    if defined, it will deploy a mock version of that contract, and `
+    return that mock contract.
+
+      Args:
+        contract_name(string)
+      Returns:
+        brownie.network.contract.ProjectContract: The most recently deployed version of this contract.
+    """
+    contract_type = contract_to_mock[contract_name]
+    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        if len(contract_type) <= 0:
+            deploy_mocks()
+        contract = contract_type[-1]
+    else:
+        contract_address = config["networks"][network.show_active()][contract_name]
+        contract = Contract.from_abi(
+            contract_type._name, contract_address, contract_type.abi
+        )
+    return contract
+
+
+def deploy_mocks(decimals=DECIMALS, initial_value=INITIAL_VALUE):
+    account = get_account()
+    MockV3Aggregator.deploy(decimals, initial_value, {"from": account})
+    link_token = LinkToken.deploy({"from": account})
+    VRFCoordinatorMock.deploy(link_token.address, {"from": account})
+    print("Deployed!")
