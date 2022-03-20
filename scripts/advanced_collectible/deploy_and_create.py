2f5abad92b51b78460f4d7f981e1b1df5083b31f
Bi Wu <alerk_star@outlook.com>
2022-03-21 02:19:04 +0800

    added AdvancedCollectible;

---
+++ scripts/advanced_collectible/deploy_and_create.py
@@ -0,0 +1,18 @@
+from scripts.helpful_scripts import get_account
+from brownie import SimpleCollectible
+
+sample_token_uri = "ipfs://Qmd9MCGtdVz2miNumBHDbvj8bigSgTwnr4SbyH6DNnpWdt?filename=0-PUG.json"
+OPENSEA_URL = "https://testnets.opensea.io/assets/{}/{}"
+
+
+def main():
+  deploy_and_create()
+
+
+def deploy_and_create():
+  account = get_account()
+  simple_collectible = SimpleCollectible.deploy({"from": account})
+  tx = simple_collectible.createCollectible(sample_token_uri, {"from": account})
+  tx.wait(1)
+  print(f"Awesome, you can view your NFT at {OPENSEA_URL.format(simple_collectible.address, simple_collectible.tokenCounter() - 1)}")
+  return simple_collectible
