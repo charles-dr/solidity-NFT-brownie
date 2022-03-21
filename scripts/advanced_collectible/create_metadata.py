4e6323a9192d3a1ef48c576884d44529a0476cb1
Bi Wu <alerk_star@outlook.com>
2022-03-21 09:36:04 +0800

    upload image to ipfs;

--- scripts/advanced_collectible/create_metadata.py
+++ scripts/advanced_collectible/create_metadata.py
@@ -2,6 +2,7 @@ from brownie import AdvancedCollectible, network
 from scripts.helpful_scripts import get_breed
 from metadata.sample_metadata import metadata_template
 from pathlib import Path
+import requests
 
 def main():
   advanced_collectible = AdvancedCollectible[-1]
@@ -15,7 +16,22 @@ def main():
       print(f"{metadata_file_name} already exists! Delete it to overwrite")
     else:
       print(f"Creating metadata file: {metadata_file_name}")
-      collectible_metadata.name = breed
-      collectible_metadata.description = f"An adorable {breed} pup(by BW)!"
+      collectible_metadata['name'] = breed
+      collectible_metadata['description'] = f"An adorable {breed} pup(by BW)!"
+      image_path = "./img/" + breed.lower().replace("_", "-") + ".png"
+      image_uri = upload_to_ipfs(image_path)
+      # collectible_metadata.image = image_uri
 
 
+def upload_to_ipfs(filepath):
+  with Path(filepath).open('rb') as fp:
+    image_binary = fp.read()
+    # upload stuff...
+    ipfs_url = "http://127.0.0.1:5001"
+    endpoint = "/api/v0/add"
+    response = requests.post(ipfs_url + endpoint, files={"file": image_binary})
+    ipfs_hash = response.json()['Hash']
+    filename = filepath.split("/")[-1:][0]
+    image_uri = f"https://ipfs.io/ipfs/{ipfs_hash}?filename={filename}"
+    print(f"[Filename] {image_uri}, {response.json()['Name']}")
+    return image_uri
